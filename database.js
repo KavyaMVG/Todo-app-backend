b536541ba39a4bd159196d2bd1233f74aa42f365
kavyaMVG <kavya.mvg@gmail.com>
2025-05-02 11:02:27 +0530

    Added travel advisor project

--- database.js
+++ database.js
@@ -4,10 +4,13 @@ const dotenv = require("dotenv");
 const URL = process.env.MONGO_URL;
 const connect = async () => {
   console.log("Connecting to db....");
-  mongoose.connect(URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }).then(() => console.log("DB connected")).catch(err => console.log("Failed to connect to DB: ",err))
+  mongoose
+    .connect(URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB connected"))
+    .catch((err) => console.log("Failed to connect to DB: ", err));
 };
 
 exports.connect = connect;
